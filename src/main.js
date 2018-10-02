dbf27b59c1e25d05faa5d17e4379aae4e53916a4
YangNTTU <38778201+YangNTTU@users.noreply.github.com>
2018-10-02 15:05:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import equipment_style from './css/equipment.scss';
 import promote_style from './css/promote.scss';
 import contact_style from './css/contact.scss';
 import tech_style from './css/tech.scss';
-import pen_style from './css/pen.scss';
 import html from '../index.html';
 import Router from './router.js';
 import Map from './javascript/osm.js';
